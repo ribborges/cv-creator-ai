1a941922320f5172ede520f2b89bd630bca04fc7
Richard Borges <ribborges@outlook.com>
2025-02-22 01:46:17 -0300

    fix: change POST to GET method for /ai route

--- src/routes/ai.ts
+++ src/routes/ai.ts
@@ -3,5 +3,5 @@ import { Router } from "express";
 import { getData } from "@/controllers/ai";
 
 export default (router: Router) => {
-    router.post("/ai", getData);
+    router.get("/ai", getData);
 };
